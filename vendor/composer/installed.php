b6e2329cee91ce87c58f0f29558c1d2a02be1613
Israt Ahamed Sabbir <93650049+ahamedsabbir@users.noreply.github.com>
2024-01-27 21:25:19 +0600

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wdpf/pagination',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b70c9d080bd00909ea881c7201050a5e0893e182',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wdpf/pagination' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b70c9d080bd00909ea881c7201050a5e0893e182',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
