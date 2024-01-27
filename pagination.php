78e95229acc5036e6644ca0d58bc6f5ba3affde9
Israt Ahamed Sabbir <93650049+ahamedsabbir@users.noreply.github.com>
2024-01-27 11:48:35 +0600

    Update pagination.php

--- pagination.php
+++ pagination.php
@@ -72,22 +72,4 @@ class pagination{
         echo "<li class='page-item {$next_disabled}'><a class='page-link' href='?{$this->pageName}={$this->totalRows}{$this->link}'>Last</a></li>";
     }
 }
-/*
-# make object
-use vendor\pagination as pagi;
-$pagination = new pagi\pagination();
-or
-$pagination = new vendor\pagination\pagination();
-
-# for server page
-$pagination->backend('p', 20);
-$totalRecord = SELECT * FROM `product` LIMIT $pagination->beganRow, $pagination->rowsSize;
-
-# for clint site view
-$pagination->frontend($totalRecord, 'p', 20)->pagination();
-
-# multiple pagenation system
-$page = "&page=".$_GET["page"];
-$pagination->frontend($totalRecord, 'p', 20, $page)->bootstrap();
-*/
 ?>
